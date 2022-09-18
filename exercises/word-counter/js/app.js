86b31eef2341c84f2f370ae769a2f84945c92ec3
kenkelbt <kenkelbt@mail.uc.edu>
2022-09-18 11:32:32 -0400

    completed word-counter

--- exercises/word-counter/js/app.js
+++ exercises/word-counter/js/app.js
@@ -1,7 +1,25 @@
 // 🤖: create 2 constants with references to textarea input and the stats section from the DOM
+const text = document.getElementById("text");
+const stat = document.getElementById("stat");
 
-
-//txtAreaInput.innerHTML = "blah";
 // 🤖: Create an Event Listener on textarea input
 // 🤖: The event handler should update the stats section with the number of words and characters in the textarea input.
 // 🤖: You will need to utilize the split method to get the words count.
+
+var wordCount = 0;
+var charCount = 0;
+text.addEventListener('input', function(){
+
+  if(text.value == false)
+  {
+    wordCount = 0;
+    charCount = 0;
+  }
+  else
+  {
+    wordCount = text.value.trim().replace(/\s+/g, ' ').split(' ').length;
+    charCount = text.value.split('').length;
+  }
+  stat.innerHTML = `You've written ${wordCount} words and ${charCount} characters.`;
+
+});
