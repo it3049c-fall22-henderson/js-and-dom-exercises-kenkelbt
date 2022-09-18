9048824f3dcebeb767184173993deef64fae7378
kenkelbt <kenkelbt@mail.uc.edu>
2022-09-18 12:26:51 -0400

    completed array-exercises

--- exercises/array-exercises/js/array.js
+++ exercises/array-exercises/js/array.js
@@ -8,7 +8,7 @@ class ArrayFunctions {
    * indexOf([1,2,3,5,6], 3); // 2
    */
   indexOf(arr, item) {
-    return ;
+    return arr.indexOf(item);
   }
 
   /**
@@ -20,7 +20,9 @@ class ArrayFunctions {
    */
   sum(arr) {
     // Hint: use the reduce method
-    return ;
+    return arr.reduce(function(prevVal, curVal) {
+      return prevVal + curVal;
+    });
   }
 
   /**
@@ -33,7 +35,9 @@ class ArrayFunctions {
    * filter([1,2,3,5,6,2,4,2], 2); // [1,3,5,6,4]
    */
   filterOut(arr, item) {
-    return ;
+    return arr.filter(function(x) {
+      return x !== item;
+    });
   }
 
   /**
@@ -46,6 +50,7 @@ class ArrayFunctions {
    */
   append(arr, item) {
     // hint: push, slice, splice would all work. Pay attention to the return value in the docs
+    arr.push(item);
     return arr;
   }
 
@@ -58,7 +63,7 @@ class ArrayFunctions {
    * concat([1,2,3], [4,5,6]); // [1,2,3,4,5,6]
    */
   concat(arr1, arr2) {
-    return ;
+    return arr1.concat(arr2);
   }
 
   /**
@@ -70,6 +75,8 @@ class ArrayFunctions {
    */
   square(arr) {
     // hint: use .map
-    return ;
+    return arr.map(function(num) {
+      return num * num;
+    });
   }
 }
